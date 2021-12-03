7d285dbcb48a1bcd32669e2d201dafcf688d9789
Anna Lehto <ankaleh@users.noreply.github.com>
2021-12-03 09:17:13 +0200

    Exercise 12.15: Development in containers

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = 'http://localhost:3000'|| process.env.REACT_APP_BACKEND_URL
+
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000'//process.env.REACT_APP_BACKEND_URL,
+  baseURL
 })
 
 export default apiClient
